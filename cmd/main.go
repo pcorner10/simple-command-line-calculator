7205c8a3c580ca336b7bfa02f2ee18aebdfb9370
Pedro Rincon Avalos <princon04.20@gmail.com>
2023-01-23 06:20:39 -0600

    Mini project completeed

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,22 @@ import (
 	"strconv"
 
 	"github.com/pcorner10/simple-command-line-calculator/operations/div"
+	"github.com/pcorner10/simple-command-line-calculator/operations/multiply"
+	"github.com/pcorner10/simple-command-line-calculator/operations/substract"
+	"github.com/pcorner10/simple-command-line-calculator/operations/sums"
 )
 
 func main() {
 	fmt.Println("Project running...")
 
+	var result float64
+	var err error
+	var succes bool = true
+
 	args := os.Args[1:]
 
-	if len(args) != 2 {
+	if len(args) < 2 {
+		succes = false
 		fmt.Println("Usage: go run cmd/main.go [operation] [list]")
 	}
 
@@ -21,19 +29,29 @@ func main() {
 
 	listNumbers := stringsToFloats(args[1:])
 
-	var result float64
-	var err error
-
 	switch operation {
 	case "div":
 		result, err = div.Division(listNumbers)
+	case "sum":
+		result, err = sums.Sums(listNumbers)
+	case "subs":
+		result, err = substract.Substract(listNumbers)
+	case "multi":
+		result, err = multiply.Multiply(listNumbers)
+	default:
+		succes = false
+		fmt.Println("Invalid input: Operation unknown")
+		fmt.Println("Can be: div, sum, subs or multi")
 	}
 
 	if err != nil {
+		succes = false
 		fmt.Println(err)
 	}
 
-	fmt.Printf("The result of: %s is %f", operation, result)
+	if succes {
+		fmt.Printf("The result of: %s is %f", operation, result)
+	}
 
 }
 
